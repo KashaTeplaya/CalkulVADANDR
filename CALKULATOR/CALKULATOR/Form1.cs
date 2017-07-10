0e2bc8d3f001a659840fb1b8f6cabf2c51a632d5
KashaTeplaya <bachanow@mail.ru>
2017-07-10 17:22:18 +0600

    Common method calkulator

--- CALKULATOR/CALKULATOR/Form1.cs
+++ CALKULATOR/CALKULATOR/Form1.cs
@@ -28,26 +28,47 @@ namespace CALKULATOR
 
         private void button3_Click(object sender, EventArgs e)
         {
-            double fisrtNumber = Convert.ToDouble(textBox1.Text);
-            double secondNumber = Convert.ToDouble(textBox2.Text);
-            double result = fisrtNumber - secondNumber;
-            textBox3.Text = result.ToString();
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            double fisrtNumber = Convert.ToDouble(textBox1.Text);
-            double secondNumber = Convert.ToDouble(textBox2.Text);
-            double result = fisrtNumber / secondNumber;
-            textBox3.Text = result.ToString();
+
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+
+        }
+        private void Global_Click(object sender, EventArgs e)
+        {
             double fisrtNumber = Convert.ToDouble(textBox1.Text);
             double secondNumber = Convert.ToDouble(textBox2.Text);
-            double result = fisrtNumber * secondNumber;
+            double result;
+            switch (((Button)sender).Name)
+            {
+                case "button1":
+                    //выполнение операции
+                    result = fisrtNumber + secondNumber;
+                    break;
+                case "button2":
+                   result = fisrtNumber - secondNumber;
+                    //выполнение операции
+                    break;
+                case "button3":
+                    result = fisrtNumber / secondNumber;
+                    break;
+                case "button4" :
+                   result = fisrtNumber * secondNumber;
+                    break;
+                default:
+                    throw new Exception("unknown operation");
+                   
+            }
             textBox3.Text = result.ToString();
+
+
         }
+
     }
 }
